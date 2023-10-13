d4dd3d79be1e179e303675435bf150e7fec817e6
Ali Yazdi <ma.yazdi75@gmail.com>
2023-10-13 02:49:44 -0400

    send a chat to firebase

---
+++ lib/chat_message.dart
@@ -0,0 +1,26 @@
+class ChatMessage {
+  ChatMessage({
+    required this.time,
+    required this.message,
+  });
+
+  final int time;
+  final String message;
+
+  String get timestamp =>
+      '${DateTime.fromMillisecondsSinceEpoch(time).hour}:${DateTime.fromMillisecondsSinceEpoch(time).minute}';
+
+  factory ChatMessage.fromJson(Map<String, dynamic> json) {
+    return ChatMessage(
+      time: json['time'],
+      message: json['message'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'time': time,
+      'chatMessage': message,
+    };
+  }
+}
